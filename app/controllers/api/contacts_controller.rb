2582f30b54283250faf647a94f0dcc0d0a945d72
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-01-17 12:24:11 -0600

    CRUD'ed up the app

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,21 +1,43 @@
 class Api::ContactsController < ApplicationController
-  def first_contact_action
-    @contact = Contact.first
-    render "first_contact_view.json.jb"
-  end
-
-  def second_contact_action
-    @contact = Contact.second
-    render "second_contact_view.json.jb"
-  end
-
-  def third_contact_action
-    @contact = Contact.third
-    render "third_contact_view.json.jb"
-  end
-
-  def all_contacts_action
+  def index
     @contacts = Contact.all
-    render "all_contacts_view.json.jb"
+    render "index.json.jb"
   end
+
+  def create
+    @contact = Contact.new(
+                          {
+                            first_name: params[:first_name],
+                            last_name: params[:last_name],
+                            email: params[:email],
+                            phone_number: params[:phone_number]
+                          }
+                            )
+    @contact.save
+    render 'show.json.jb'
+  end
+
+  def show
+    @contact = Contact.find(params[:id]) 
+    render "show.json.jb"   
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.delete
+    render json: {message: "Contact ID #{params[:id]} successfully deleted!"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/first_contact_url' => 'contacts#first_contact_action'
-    get '/second_contact_url' => 'contacts#second_contact_action'
-    get '/third_contact_url' => 'contacts#third_contact_action'
-    get '/all_contacts_url' => 'contacts#all_contacts_action'
+    get '/contacts' => 'contacts#index'
+    post '/contacts' => 'contacts#create'
+    get '/contacts/:id' => 'contacts#show'
+    patch '/contacts/:id' => 'contacts#update'
+    delete '/contacts/:id' => 'contacts#destroy'
   end
 end
